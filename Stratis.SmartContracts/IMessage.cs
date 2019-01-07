9918af17662f640abda328cb1f4855014e9bdaa4
Jordan Andrews <jordandrews@hotmail.com>
2019-01-07 14:28:01 +1100

    Transferring code from StratisBitcoinFullNode

---
+++ Stratis.SmartContracts/IMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace Stratis.SmartContracts
+{
+    public interface IMessage
+    {
+        /// <summary>
+        /// The address of the contract currently being executed.
+        /// </summary>
+        Address ContractAddress { get; }
+
+        /// <summary>
+        /// The address that called this contract.
+        /// </summary>
+        Address Sender { get; }
+
+        /// <summary>
+        /// The amount of STRAT sent in this call. 
+        /// </summary>
+        ulong Value { get; }
+    }
+}
---
+++ Stratis.SmartContracts/ISmartContractState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stratis.SmartContracts
+{
+    public interface ISmartContractState
+    {
+        IBlock Block { get; }
+        IMessage Message { get; }
+        IPersistentState PersistentState { get; }
+        IGasMeter GasMeter { get; }
+        IContractLogger ContractLogger { get; }
+        IInternalTransactionExecutor InternalTransactionExecutor { get; }
+        IInternalHashHelper InternalHashHelper { get; }
+        ISerializer Serializer { get; }
+        Func<ulong> GetBalance { get; }
+    }
+}
