c09f8307728ddf0c1eb45f5317722642772c3a86
Jordan Andrews <jordandrews@hotmail.com>
2019-02-19 16:27:15 +1100

    Add units to doc
    Co-Authored-By: rowandh <rowandh@users.noreply.github.com>

--- Stratis.SmartContracts/IMessage.cs
+++ Stratis.SmartContracts/IMessage.cs
@@ -13,7 +13,7 @@
         Address Sender { get; }
 
         /// <summary>
-        /// The number of coins sent with this call.
+        /// The number of coins sent with this call (in satoshis).
         /// </summary>
         ulong Value { get; }
     }
