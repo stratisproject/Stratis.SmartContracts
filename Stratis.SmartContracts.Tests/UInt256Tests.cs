5b81e10134f4f86b6868799b82347b3f64a2439e
quantumagi <someguy.fromafrica@gmail.com>
2021-01-12 19:53:58 +1100

    Add more tests

--- Stratis.SmartContracts.Tests/UInt256Tests.cs
+++ Stratis.SmartContracts.Tests/UInt256Tests.cs
@@ -25,5 +25,62 @@ namespace Stratis.SmartContracts.Tests
             UInt256 x = UInt256.Parse("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
             Assert.Equal(x, new UInt256(x.ToBytes()));
         }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void CanAdd()
+        {
+            UInt256 v1 = new UInt256("0000000000000000000000000000000000000000000000000000000000000325");
+            UInt256 v2 = new UInt256("0000000000000000000000000000000000000000000000000000000000000F0F");
+            UInt256 v3 = v1 + v2;
+
+            Assert.Equal(new UInt256("0000000000000000000000000000000000000000000000000000000000001234"), v3);
+        }
+
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void CanSubtract()
+        {
+            UInt256 v1 = new UInt256("0000000000000000000000000000000000000000000000000000000000001234");
+            UInt256 v2 = new UInt256("0000000000000000000000000000000000000000000000000000000000000325");
+            UInt256 v3 = v1 - v2;
+
+            Assert.Equal(new UInt256("0000000000000000000000000000000000000000000000000000000000000F0F"), v3);
+        }
+
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void CanMultiply()
+        {
+            UInt256 v1 = new UInt256("0000000000000000000000000000000000000000000000000000000000001234");
+            UInt256 v2 = new UInt256("0000000000000000000000000000000000000000000000000000000000005678");
+            UInt256 v3 = v1 * v2;
+
+            Assert.Equal(new UInt256("0000000000000000000000000000000000000000000000000000000006260060"), v3);
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void CanDivide()
+        {
+            UInt256 v1 = new UInt256("0000000000000000000000000000000000000000000000000000000006260060");
+            UInt256 v2 = new UInt256("0000000000000000000000000000000000000000000000000000000000005678");
+            UInt256 v3 = v1 / v2;
+
+            Assert.Equal(new UInt256("0000000000000000000000000000000000000000000000000000000000001234"), v3);
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void CanParseLargeNumbers()
+        {
+            UInt256 v1 = UInt256.Parse("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
+            UInt256 v2 = UInt256.Parse("0000000000000000000000000000000ebaaedce6af48a03bbfd25e8cd0364141");
+            UInt256 v3 = v1 - v2;
+
+            Assert.Equal(new UInt256("fffffffffffffffffffffffffffffff000000000000000000000000000000000"), v3);
+        }
     }
 }
