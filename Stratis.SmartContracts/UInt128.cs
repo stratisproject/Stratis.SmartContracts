3fbe429f4eacd8f9b97478b482e6a6c18dbb63dd
quantumagi <someguy.fromafrica@gmail.com>
2021-01-12 18:28:42 +1100

    Rename

--- Stratis.SmartContracts/UInt128.cs
+++ Stratis.SmartContracts/UInt128.cs
@@ -2,99 +2,99 @@
 
 namespace Stratis.SmartContracts
 {
-    public class uint128 : BigIntegerBase
+    public class UInt128 : BigIntegerBase
     {
         const int WIDTH = 16;
 
-        public uint128() : base(WIDTH)
+        public UInt128() : base(WIDTH)
         {
         }
 
-        public uint128(BigInteger value) : base(WIDTH, value)
+        public UInt128(BigInteger value) : base(WIDTH, value)
         {
         }
 
-        public uint128(uint128 value) : this(value.value)
+        public UInt128(UInt128 value) : this(value.value)
         {
         }
 
-        public uint128(string hex) : base(WIDTH, hex)
+        public UInt128(string hex) : base(WIDTH, hex)
         {
         }
 
-        public static uint128 Parse(string hex)
+        public static UInt128 Parse(string hex)
         {
-            return new uint128(hex);
+            return new UInt128(hex);
         }
 
-        public uint128(ulong b) : base(WIDTH, b)
-        {
-        }
-
-        public uint128(byte[] vch, bool lendian = true) : base(WIDTH, vch, lendian)
+        public UInt128(ulong b) : base(WIDTH, b)
         {
         }
 
-        public static uint128 operator >>(uint128 a, int shift)
-        {
-            return new uint128(a.ShiftRight(shift));
-        }
-
-        public static uint128 operator <<(uint128 a, int shift)
-        {
-            return new uint128(a.ShiftLeft(shift));
-        }
-
-        public static uint128 operator -(uint128 a, uint128 b)
-        {
-            return new uint128(a.Subtract(b.value));
-        }
-
-        public static uint128 operator +(uint128 a, uint128 b)
-        {
-            return new uint128(a.Add(b.value));
-        }
-
-        public static uint128 operator *(uint128 a, uint128 b)
-        {
-            return new uint128(a.Multiply(b.value));
-        }
-
-        public static uint128 operator /(uint128 a, uint128 b)
-        {
-            return new uint128(a.Divide(b.value));
-        }
-
-        public static uint128 operator %(uint128 a, uint128 b)
-        {
-            return new uint128(a.Mod(b.value));
-        }
-
-        public uint128(byte[] vch) : this(vch, true)
+        public UInt128(byte[] vch, bool lendian = true) : base(WIDTH, vch, lendian)
         {
         }
 
-        public static bool operator <(uint128 a, uint128 b)
+        public static UInt128 operator >>(UInt128 a, int shift)
+        {
+            return new UInt128(a.ShiftRight(shift));
+        }
+
+        public static UInt128 operator <<(UInt128 a, int shift)
+        {
+            return new UInt128(a.ShiftLeft(shift));
+        }
+
+        public static UInt128 operator -(UInt128 a, UInt128 b)
+        {
+            return new UInt128(a.Subtract(b.value));
+        }
+
+        public static UInt128 operator +(UInt128 a, UInt128 b)
+        {
+            return new UInt128(a.Add(b.value));
+        }
+
+        public static UInt128 operator *(UInt128 a, UInt128 b)
+        {
+            return new UInt128(a.Multiply(b.value));
+        }
+
+        public static UInt128 operator /(UInt128 a, UInt128 b)
+        {
+            return new UInt128(a.Divide(b.value));
+        }
+
+        public static UInt128 operator %(UInt128 a, UInt128 b)
+        {
+            return new UInt128(a.Mod(b.value));
+        }
+
+        public UInt128(byte[] vch) : this(vch, true)
+        {
+        }
+
+        public static bool operator <(UInt128 a, UInt128 b)
         {
             return Comparison(a, b) < 0;
         }
 
-        public static bool operator >(uint128 a, uint128 b)
+        public static bool operator >(UInt128 a, UInt128 b)
         {
             return Comparison(a, b) > 0;
         }
 
-        public static bool operator <=(uint128 a, uint128 b)
+        public static bool operator <=(UInt128 a, UInt128 b)
         {
             return Comparison(a, b) <= 0;
         }
 
-        public static bool operator >=(uint128 a, uint128 b)
+        public static bool operator >=(UInt128 a, UInt128 b)
         {
             return Comparison(a, b) >= 0;
         }
 
-        public static bool operator ==(uint128 a, uint128 b)
+        public static bool operator ==(UInt128 a, UInt128 b)
         {
             if (ReferenceEquals(a, b))
                 return true;
@@ -105,24 +105,24 @@ namespace Stratis.SmartContracts
             return Comparison(a, b) == 0;
         }
 
-        public static bool operator !=(uint128 a, uint128 b)
+        public static bool operator !=(UInt128 a, UInt128 b)
         {
             return !(a == b);
         }
 
-        public static bool operator ==(uint128 a, ulong b)
+        public static bool operator ==(UInt128 a, ulong b)
         {
-            return (a == new uint128(b));
+            return (a == new UInt128(b));
         }
 
-        public static bool operator !=(uint128 a, ulong b)
+        public static bool operator !=(UInt128 a, ulong b)
         {
-            return !(a == new uint128(b));
+            return !(a == new UInt128(b));
         }
 
-        public static implicit operator uint128(ulong value)
+        public static implicit operator UInt128(ulong value)
         {
-            return new uint128(value);
+            return new UInt128(value);
         }
     }
 }
